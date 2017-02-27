56c805d495a74d25c5ff738a2b4d5e69377b9fef
Enoch2k2 <enoch2k2@hotmail.com>
2017-02-27 14:18:10 -0600

    Almost done with lab

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 class Band extends Component {
+  constructor(props){
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(id){
+    console.log('This props store is ' + this.props.store)
+    this.props.store.dispatch({
+      type: "DELETE_BAND",
+      payload: id
+    })
+  }
   render(){
     return(
       <div>
-        band component
+        <li>{this.props.text}</li>
+        <button onClick={this.handleClick.bind(null, this.props.id)}>Delete</button>
       </div>
     )
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,7 +1,15 @@
+var id = 0;
 export default function manageBand(state = {bands: []}, action){
   switch (action.type) {
     case 'ADD_BAND':
-      return Object.assign(state, {}, {bands: state.bands.concat(action.payload)})
+      id++;
+      var band = {text: action.payload, id: id};
+      var bands = [...state.bands, band];
+      return Object.assign({}, state, {bands: bands});
+    case "DELETE_BAND":
+      var bands = [...state.bands];
+      bands.splice(action.payload, 1);
+      return Object.assign({}, state, {bands: bands});
     default:
       return state;
   }
